8c4d831d3e1db09bed25ace0257f482a2d1b13fa
letsget <andrewbond25@gmail.com>
2020-11-17 12:18:45 +0200

    Updates

--- src/App/App.tsx
+++
@@ -1,7 +0,0 @@
-import React, { FC } from 'react';
-import ProductsPage from '../pages/ProductsPage';
-import './App.css';
-
-const App: FC = () => <ProductsPage />;
-
-export default App;
--- src/types/index.tsx
+++
@@ -1,7 +0,0 @@
-export interface ProductTypes {
-  id?: string;
-  name: string | undefined;
-  price: string;
-  color: string;
-  img: string;
-}
